21fe6217a90965b423f3c18173589990cb497a50
Andrew-coder-bit <147925103+Andrew-coder-bit@users.noreply.github.com>
2023-10-14 16:12:32 +0300

    Add files via upload

---
+++ лаб. р. №1/Task_1.py
@@ -0,0 +1,12 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+
+for i in range(len(numbers)):
+    try:
+        int(numbers[i])
+    except:
+        numbers[i] = 0
+        numbers[i] = sum(numbers)/len(numbers)
+
+print("Измененный список:", numbers)
---
+++ лаб. р. №1/Task_2.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+lenght = len(list_players)//2
+
+print(list_players[:lenght])
+print(list_players[lenght:])
