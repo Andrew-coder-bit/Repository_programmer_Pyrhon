79c19dd86d1d12b4a29e81da19d09d92fe3925d7
Andrew-coder-bit <147925103+Andrew-coder-bit@users.noreply.github.com>
2023-11-16 12:29:22 +0300

    Update Task1.py

--- Лаб.р.№4/Task1.py
+++ Лаб.р.№4/Task1.py
@@ -4,13 +4,8 @@ def task() -> float:
     with open('input.json', 'r') as file:
         data = json.load(file)
 
-    total = 0
-    for item in data:
-        score = item['score']
-        weight = item['weight']
-        product = score * weight
-        total += product
-
+    total = sum(item['score'] * item['weight'] for item in data) #использование генератора
     return round(total, 3)
+
 
 print(task())
